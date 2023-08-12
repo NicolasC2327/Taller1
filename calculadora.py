d270f8d9216dfed48fa23f500df16374ccd9bd52
nicolas <RatatoskrBRHU@gmail.com>
2023-08-12 11:02:09 -0500

    Calculadora(divison)

--- calculadora.py
+++ calculadora.py
@@ -1,6 +1,6 @@
-valorUno = int(input("Ingresa el primer valor el cual quieras multiplicar"))
-valorDos = int(input("Ingresa el segundo valor el cual quieras multiplicar con el primero"))
+valorUno = float(input("Ingresa el primer valor el cual quieras dividir"))
+valorDos = float(input("Ingresa el segundo valor el cual quieras dividir con el primero"))
 
-valorTotal = valorUno * valorDos
+valorTotal = valorUno / valorDos
 
-print(f"La sumatoria de esos dos valores es {valorTotal}")
+print(f"La division de esos dos valores es {valorTotal}")
