072b0f2d1f282a0f5fc48e8312cf870d6b8c67a7
nicolas <RatatoskrBRHU@gmail.com>
2023-08-12 10:55:44 -0500

    Calculadora(resta)

--- calculadora.py
+++ calculadora.py
@@ -1,6 +1,6 @@
-valorUno = int(input("Ingresa el primer valor el cual quieras sumar"))
-valorDos = int(input("Ingresa el segundo valor el cual quieras sumar con el primero"))
+valorUno = int(input("Ingresa el primer valor el cual quieras restar"))
+valorDos = int(input("Ingresa el segundo valor el cual quieras restar con el primero"))
 
-valorTotal = valorUno + valorDos
+valorTotal = valorUno - valorDos
 
 print(f"La sumatoria de esos dos valores es {valorTotal}")
