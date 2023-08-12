c3502c3bbaf4d96d595904d95013d6d5548c1382
nicolas <RatatoskrBRHU@gmail.com>
2023-08-12 10:59:50 -0500

    Calculadora(multiplicar)

--- calculadora.py
+++ calculadora.py
@@ -1,6 +1,6 @@
-valorUno = int(input("Ingresa el primer valor el cual quieras restar"))
-valorDos = int(input("Ingresa el segundo valor el cual quieras restar con el primero"))
+valorUno = int(input("Ingresa el primer valor el cual quieras multiplicar"))
+valorDos = int(input("Ingresa el segundo valor el cual quieras multiplicar con el primero"))
 
-valorTotal = valorUno - valorDos
+valorTotal = valorUno * valorDos
 
 print(f"La sumatoria de esos dos valores es {valorTotal}")
