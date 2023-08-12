18b411aa84f731383549d138eb33ea76f70b734c
nicolas <RatatoskrBRHU@gmail.com>
2023-08-12 10:46:53 -0500

    calculadora.py

---
+++ calculadora.py
@@ -0,0 +1,6 @@
+valorUno = input("Ingresa el primer valor el cual quieras sumar")
+valorDos = input("Ingresa el segundo valor el cual quieras sumar con el primero")
+
+valorTotal = valorUno + valorDos
+
+print(f"La sumatoria de esos dos valores es {valorTotal}")
